be7967f5ba4822fa5227833a7cb412c53f4b56f3
kenjura <github@kenjura.com>
2019-04-04 14:30:05 -0700

    removed unnecessary code

--- src/api/services/dropbox.js
+++ src/api/services/dropbox.js
@@ -11,31 +11,6 @@ module.exports = { getArticle };
 // TODO: apply order of preference for file types (or at least yell at the user for having duplicates)
 // TODO: do away with db and path, and just use location (maybe?)
 
-async function getArticleOld(db, path) {
-	const dropbox = new Dropbox({ accessToken: process.env.DROPBOX_ACCESS_TOKEN, fetch: fetch });
-
-	const fileList = await getFileList(db, dropbox);
-
-	// for now, let's just assume style and menu are in the root, and grab the first match TODO: make this work according to designed logic
-	const [ menu, style, article ] = await Promise.all([ 
-		getMenu({ db, path, fileList, dropbox }),
-		getStyle({ db, path, fileList, dropbox }),
-		getArticleContent({ db, path, fileList, dropbox }),
-	]);
-
-	return { menu, style, article };
-
-	// const path = `/${process.env.DROPBOX_ROOT}/${req.params[0]}`;
-	// debug(`attempting to load path ${path} from Dropbox...`);
-	// try {
-	// 	const response = await dropbox.filesDownload({ path });
-	// 	const fileContent = response.fileBinary.toString('utf8');
-	// 	res.send(fileContent);
-	// } catch(err) {
-	// 	console.error(err);
-	// 	res.send(err);
-	// }
-}
 async function getArticle(db, articlePath) {
 	const dropbox = new Dropbox({ accessToken: process.env.DROPBOX_ACCESS_TOKEN, fetch: fetch });
 
@@ -52,33 +27,21 @@ async function getArticle(db, articlePath) {
 
 }
 
-async function getFileList(db, dropbox) {
-	if (!dropbox) throw new Error('dropbox service > getFileList > initialized dropbox instance must be supplied.');
+// async function getFileList(db, dropbox) {
+// 	if (!dropbox) throw new Error('dropbox service > getFileList > initialized dropbox instance must be supplied.');
 
-	const path = `/${process.env.DROPBOX_ROOT}/${db}`;
-	const recursive = true;
+// 	const path = `/${process.env.DROPBOX_ROOT}/${db}`;
+// 	const recursive = true;
 
-	debug(`getFileList > attempting to load file list for db "${db}". path="${path}"`);
+// 	debug(`getFileList > attempting to load file list for db "${db}". path="${path}"`);
 
-	const response = await dropbox.filesListFolder({ path, recursive });
-	const { entries } = response;
-	const fileList = entries.map(entry => entry.path_lower);
-	debug('getFileList > fileList = ',fileList);
-	return fileList;
-}
+// 	const response = await dropbox.filesListFolder({ path, recursive });
+// 	const { entries } = response;
+// 	const fileList = entries.map(entry => entry.path_lower);
+// 	debug('getFileList > fileList = ',fileList);
+// 	return fileList;
+// }
 
-async function getArticleContent({ db, fileList, path, dropbox }) {
-	if (!dropbox) throw new Error('dropbox service > getArticle > initialized dropbox instance must be supplied.');
-
-
-	const articlePath = fileList.find(file => file.match(path));
-	debug(`getArticleContent > attempting to load article content for db "${db}" and path "${path}". articlePath="${articlePath}"`);
-	if (!articlePath) throw new Error(`dropbox > getArticleContent > no file found matching path "${path}"`);
-	const response = await dropbox.filesDownload({ path:articlePath });
-	const fileContent = response.fileBinary.toString('utf8');
-
-	return fileContent;
-}
 
 async function getFileByPath({ db, dropbox, articlePath }) {
 	if (!dropbox) throw new Error('dropbox service > getFileByPath > initialized dropbox instance must be supplied.');
@@ -161,28 +124,4 @@ async function getFileByPath({ db, dropbox, articlePath }) {
 	}
 
 
-}
-
-async function getMenu({ db, fileList, path, dropbox }) {
-	if (!dropbox) throw new Error('dropbox service > getMenu > initialized dropbox instance must be supplied.');
-
-	debug(`getMenu > attempting to load menu content for db "${db}" and path "${path}"`);
-
-	const menuPath = fileList.find(file => file.match('_menu')); // TODO: look for closest file, apply extension preference, etc
-	const response = await dropbox.filesDownload({ path:menuPath });
-	const fileContent = response.fileBinary.toString('utf8');
-
-	return fileContent;
-}
-
-async function getStyle({ db, fileList, path, dropbox }) {
-	if (!dropbox) throw new Error('dropbox service > getStyle > initialized dropbox instance must be supplied.');
-
-	debug(`getStyle > attempting to load style content for db "${db}" and path "${path}"`);
-
-	const stylePath = fileList.find(file => file.match('_style')); // TODO: look for closest file, apply extension preference, etc
-	const response = await dropbox.filesDownload({ path:stylePath });
-	const fileContent = response.fileBinary.toString('utf8');
-
-	return fileContent;
 }
